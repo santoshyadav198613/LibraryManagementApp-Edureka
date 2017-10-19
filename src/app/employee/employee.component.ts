3bcef8bae5f8db00c70b0dad103ee1d1003be9c1
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-10-19 09:01:13 +0530

    added basic directives and binding examples.

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit {
+export class EmployeeComponent {
+
+  empName = 'Pavan';
+  isVisible: boolean = false;
+  empRole: string = 'Admin';
+  empList: any[] = [
+    { id: 1, name: 'Venkata', address: 'Mumbai' },
+    { id: 2, name: 'Dileep', address: 'Pune' },
+    { id: 3, name: 'Anu', address: 'Banglore' }
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  toggle() {
+    this.isVisible = !this.isVisible;
   }
 
 }
